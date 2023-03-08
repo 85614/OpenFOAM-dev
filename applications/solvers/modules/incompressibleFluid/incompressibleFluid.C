daa63ab2df7ed4e7a52d3422366365a196350539
ZhangZhiqiang <2510406196@qq.com>
2023-03-08 09:47:21 +0800

    add origin momentumTransport_t::correct

--- applications/solvers/modules/incompressibleFluid/incompressibleFluid.C
+++ applications/solvers/modules/incompressibleFluid/incompressibleFluid.C
@@ -229,9 +229,171 @@ void Foam::solvers::incompressibleFluid::postCorrector()
     }
 }
 
+#if 0
+void Foam::solvers::momentumTransport_t::correct()
+{
+    if (!this->turbulence_)
+    {
+        return;
+    }
+
+    // Local references
+    const alphaField& alpha = this->alpha_;
+    const rhoField& rho = this->rho_;
+    const surfaceScalarField& alphaRhoPhi = this->alphaRhoPhi_;
+    const volVectorField& U = this->U_;
+    volScalarField& nut = this->nut_;
+    const Foam::fvModels& fvModels(Foam::fvModels::New(this->mesh_));
+    const Foam::fvConstraints& fvConstraints
+    (
+        Foam::fvConstraints::New(this->mesh_)
+    );
+
+    eddyViscosity<RASModel<BasicMomentumTransportModel>>::correct();
+
+    volScalarField::Internal divU
+    (
+        fvc::div(fvc::absolute(this->phi(), U))()
+    );
+
+    tmp<volTensorField> tgradU = fvc::grad(U);
+    volScalarField::Internal G
+    (
+        this->GName(),
+        nut()*(dev(twoSymm(tgradU().v())) && tgradU().v())
+    );
+    tgradU.clear();
+
+    // Update epsilon and G at the wall
+    epsilon_.boundaryFieldRef().updateCoeffs();
+
+    // Dissipation equation
+    tmp<fvScalarMatrix> epsEqn
+    (
+        fvm::ddt(alpha, rho, epsilon_)
+      + fvm::div(alphaRhoPhi, epsilon_)
+      - fvm::laplacian(alpha*rho*DepsilonEff(), epsilon_)
+     ==
+        C1_*alpha()*rho()*G*epsilon_()/k_()
+      - fvm::SuSp(((2.0/3.0)*C1_ - C3_)*alpha()*rho()*divU, epsilon_)
+      - fvm::Sp(C2_*alpha()*rho()*epsilon_()/k_(), epsilon_)
+      + epsilonSource()
+      + fvModels.source(alpha, rho, epsilon_)
+    );
+
+    epsEqn.ref().relax();
+    fvConstraints.constrain(epsEqn.ref());
+    epsEqn.ref().boundaryManipulate(epsilon_.boundaryFieldRef());
+    solve(epsEqn);
+    fvConstraints.constrain(epsilon_);
+    bound(epsilon_, this->epsilonMin_);
+
+    // Turbulent kinetic energy equation
+    tmp<fvScalarMatrix> kEqn
+    (
+        fvm::ddt(alpha, rho, k_)
+      + fvm::div(alphaRhoPhi, k_)
+      - fvm::laplacian(alpha*rho*DkEff(), k_)
+     ==
+        alpha()*rho()*G
+      - fvm::SuSp((2.0/3.0)*alpha()*rho()*divU, k_)
+      - fvm::Sp(alpha()*rho()*epsilon_()/k_(), k_)
+      + kSource()
+      + fvModels.source(alpha, rho, k_)
+    );
+
+    kEqn.ref().relax();
+    fvConstraints.constrain(kEqn.ref());
+    solve(kEqn);
+    fvConstraints.constrain(k_);
+    bound(k_, this->kMin_);
+
+    correctNut();
+}
+#endif
 
 void Foam::solvers::incompressibleFluid::postSolve()
 {}
 
 
 // ************************************************************************* //
+
+#if 0
+#include <cxxabi.h>
+
+Foam::string get_typename(const std::type_info &t_info)
+{
+    char *name = abi::__cxa_demangle(t_info.name(), 0, 0, 0);
+    Foam::string res(name);
+    free(name); // 省事
+    return res;
+}
+
+
+record_t record; // record 的定义
+
+
+#include "error.H"
+#include "OStringStream.H"
+#include "OSspecific.H"
+#include "IFstream.H"
+
+#include <inttypes.h>
+#include <cxxabi.h>
+#include <execinfo.h>
+#include <dlfcn.h>
+
+namespace Foam {
+word demangleSymbol(const char* sn);
+fileName absolutePath(const char* fn);
+void printSourceFileAndLine
+(
+    Ostream& os,
+    const fileName& filename,
+    Dl_info *info,
+    void *addr
+);
+
+void Foam::Foam_error::printStack(Ostream& os)
+{
+    // Get raw stack symbols
+    const size_t CALLSTACK_SIZE = 128;
+
+    void *callstack[CALLSTACK_SIZE];
+    size_t size = backtrace(callstack, CALLSTACK_SIZE);
+
+    Dl_info *info = new Dl_info;
+
+    fileName fname = "???";
+    word address;
+
+    for(size_t i=0; i<size; i++)
+    {
+        int st = dladdr(callstack[i], info);
+
+        os << '#' << label(i) << "  ";
+        if (st != 0 && info->dli_fname != nullptr && info->dli_fname[0] != '\0')
+        {
+            fname = absolutePath(info->dli_fname);
+
+            os <<
+            (
+                (info->dli_sname != nullptr)
+              ? demangleSymbol(info->dli_sname)
+              : "?"
+            );
+        }
+        else
+        {
+            os << "?";
+        }
+
+        printSourceFileAndLine(os, fname, info, callstack[i]);
+        os << nl;
+    }
+
+    delete info;
+}
+
+} // namespace Foam
+#endif
